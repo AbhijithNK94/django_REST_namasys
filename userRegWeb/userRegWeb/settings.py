b037428734291803df1ba8d7f46c042df83acc9f
AbhijithNK94 <abhijithnandakumar5@gmail.com>
2022-01-05 10:18:55 +0530

    second commit

--- userRegWeb/userRegWeb/settings.py
+++ userRegWeb/userRegWeb/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'regApp',
+    'regApp.apps.RegappConfig',
 ]
 
 MIDDLEWARE = [
