5c866e4cf64aa29be347d1b06f4b96e9e44072fa
AbhijithNK94 <abhijithnandakumar5@gmail.com>
2022-01-05 12:38:07 +0530

    installed crispy-forms and config in settings

--- userRegWeb/userRegWeb/settings.py
+++ userRegWeb/userRegWeb/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Own
     'regApp.apps.RegappConfig',
+    # Crispy form tags
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
