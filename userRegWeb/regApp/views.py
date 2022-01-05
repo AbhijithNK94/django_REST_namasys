c56b95aa484df3b459405f8c202b7d4c67c25f46
AbhijithNK94 <abhijithnandakumar5@gmail.com>
2022-01-05 11:04:40 +0530

    development server tested successfully

--- userRegWeb/regApp/views.py
+++ userRegWeb/regApp/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 def home(request):
-    return HttpResponse("<h1>Home</h1>")
+    return render(request, 'regApp/home_pg.html')
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Hi {username}, your account was created successfully')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'regApp/register_pg.html', {'form': form})
+
+
+@login_required()
+def profile(request):
+    return render(request, 'regApp/profile_pg.html')
